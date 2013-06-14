88bfa316c0cc776d2cff16ae35d37c05b9a20e66
unknown <longdt@QS010.qsoft.com.vn>
2013-06-14 14:40:13 +0700

    Red shouldReturnExceptionWithNegativeNumber

--- src/com/qsoft/longdt/StringCalculatorTest.java
+++ src/com/qsoft/longdt/StringCalculatorTest.java
@@ -1,23 +1,35 @@
 package com.qsoft.longdt;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StringCalculatorTest {
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	@Test
 	public void shouldReturnZeroOnEmptyString() {
 		assertEquals(0, StringCalculator.add(""));
 	}
-	
+
 	@Test
 	public void shouldReturnNumberOnNumberInput() {
 		assertEquals(1, StringCalculator.add("1"));
 	}
-	
+
 	@Test
 	public void shouldReturnWithCommaDelim() {
 		assertEquals(3, StringCalculator.add("1,2"));
+	}
+
+	@Test
+	public void shouldReturnExceptionWithNegativeNumber() {
+		exception.expect(NumberFormatException.class);
+		exception.expectMessage("negatives not allowed -5");
+		StringCalculator.add("1,3,-5");
 	}
 }
