787059a625135226faf9bbf886ec002883ef9bb3
unknown <longdt@QS010.qsoft.com.vn>
2013-06-14 14:13:46 +0700

    Grren shouldReturnNumberOnNumberInput

--- src/com/qsoft/longdt/StringCalculator.java
+++ src/com/qsoft/longdt/StringCalculator.java
@@ -2,11 +2,16 @@ package com.qsoft.longdt;
 
 public class StringCalculator {
 
-	
-	public static int add(String input){
-		if(input.isEmpty()){
+	public static int add(String input) {
+		if (input.isEmpty()) {
 			return 0;
+		} else if (input.length() == 1) {
+			return toInt(input);
 		}
-		return - 1;
+		return -1;
+	}
+
+	private static int toInt(String input) {
+		return Integer.parseInt(input);
 	}
 }
